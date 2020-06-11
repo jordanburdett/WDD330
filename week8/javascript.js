3b2f4ea6d76ed6522e8deef13fd207d52f32e6e3
Jordan Burdett <jordan@burdett.us>
2020-06-11 15:51:10 -0600

    Added notes to week 8

---
+++ week8/javascript.js
@@ -0,0 +1,8 @@
+let canvas = document.getElementById("myCanvas")
+let context = canvas.getContext("2d")
+
+context.strokeStyle = "blue"
+context.fillStyle = "lightblue"
+
+context.fillRect(10,10,100,100)
+context.strokeRect(10,10,100,100)
