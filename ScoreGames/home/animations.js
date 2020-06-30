1fc8b68bba4e0b3f18855838068d9b5f61cf1e90
Jordan Burdett <jordan@burdett.us>
2020-06-30 12:48:29 -0600

    testing gsap transitions on homescreen

---
+++ ScoreGames/home/animations.js
@@ -0,0 +1,6 @@
+let timeline = gsap.timeline()
+
+timeline.from(".pageHeader", { duration: 1, y: "-100" })
+        .from(".themeToggle", {duration: 2, ease: "power4", x: -200}, "<1")
+        .from(".myGames", { duration: 1, y: -100}, 0.5)
+        .from(".editButton", { duration: 1, x: 200}, 0.5)
